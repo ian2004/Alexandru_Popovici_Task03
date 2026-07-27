dd26b958ed6fa59b842b118e4c57327f3d216787
Languini <popovici123alex75@gmail.com>
2026-07-27 23:16:54 +0300

    Model imbunatatit

--- data/predict_category.py
+++ data/predict_category.py
@@ -1,4 +1,5 @@
 import joblib
+import pandas as pd
 import requests
 from io import BytesIO
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -12,12 +13,23 @@ response.raise_for_status()
 
 model = joblib.load(BytesIO(response.content))
 
+def build_features(title):
+    return pd.DataFrame([{
+        "Product Title": title,
+        "title_len_chars": len(title),
+        "title_len_words": len(title.split()),
+        "has_digit": int(any(ch.isdigit() for ch in title)),
+        "longest_word_len": max((len(w) for w in title.split()), default=0)
+    }])
+
 while True:
     print("\n ###################### \n Modelul este pregatit! \n ######################\n")
     input_utilizator = str(input("Introduceti un item pentru a-l categoriza \n"))
     
     if input_utilizator == "exit":
+        print("######################\n Iesire din model \n ######################")
         break
     else:
-        prediction = model.predict([input_utilizator])
-        print(prediction[0])
+        X_new = build_features(input_utilizator)
+        prediction = model.predict(X_new)
+        print("######################", prediction[0], "\n######################")
